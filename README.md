fdba4389a3cfb776374c12ac6371017ecbac36b1
andyloo416 <105084947+andyloo416@users.noreply.github.com>
2022-11-24 21:56:52 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# book-search-engine
+# 21 MERN: Book Search Engine
+
+The app is to make a book search engine.
+
+When I load the search engine, it will present the options search for books and login/signup and input field to search for books with a submit button
+
+When I click the search button, I will be presented with an input field to search for books and a submit button
+
+When i click the login/signup button, a model appears with a togggle bewween the option to log in or sign up
---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  AppoloProvider,
+  AppoloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@applo/client";
+import { setContent } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+const httplink = createHttpLink((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    header: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new AppoloClient({
+  link: authLink.concat(httplink),
+  cahce: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SearchBooks />} />
+          <Route path="/saved" element={<SavedBooks />} />
+          <Route
+            path="*"
+            element={<h1 className="display-2">Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+  );
+}
+
+export default App;
