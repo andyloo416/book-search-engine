d9bf59f72c94498044ed569f18c4890fd81500e0
andyloo416 <105084947+andyloo416@users.noreply.github.com>
2022-12-04 22:43:00 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,49 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {
-  AppoloProvider,
-  AppoloClient,
+  ApolloClient,
   InMemoryCache,
+  ApolloProvider,
   createHttpLink,
-} from "@applo/client";
-import { setContent } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-const httplink = createHttpLink((_, { headers }) => {
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
   return {
-    header: {
+    headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
-const client = new AppoloClient({
-  link: authLink.concat(httplink),
-  cahce: new InMemoryCache(),
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<SearchBooks />} />
-          <Route path="/saved" element={<SavedBooks />} />
-          <Route
-            path="*"
-            element={<h1 className="display-2">Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
